5af0158ca4eae7ddc67ee9aa7bf175da71ee6689
Azad-Aihub <128484356+Azad-Aihub@users.noreply.github.com>
2025-04-05 15:53:33 +0800

    Update roles.ts

--- src/utils/roles.ts
+++ src/utils/roles.ts
@@ -2,7 +2,7 @@ export default [
   {
     role: "custom",
     avatar: "/images/custom.png",
-    fc: "请在下方设置中输入自定义system prompt",
+    fc: "Enter your custom system prompt in the Settings below",
   },
   {
 ]
